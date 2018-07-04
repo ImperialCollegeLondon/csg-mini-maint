fa19db8fd334883bea7cb66e74fcb545be1e60d3
dcw <d.white@imperial.ac.uk>
2018-07-04 21:54:09 +0100

    trying to debug weird missing hostclass bug..

--- ubuntu-16.04/lib/perl/Maint/DistSupport.pm
+++ ubuntu-16.04/lib/perl/Maint/DistSupport.pm
@@ -240,6 +240,7 @@ sub maint_distchoose ($$$)
     }
     foreach my $class (@classes)
     {
+	maint_fatalerror( "class <<$class>> missing" ) unless defined $class && $class;
         my $classfile = "$distbase$under/$class";
 	my @g = glob("$classfile.*");
 	push @g, $classfile if -f $classfile;
